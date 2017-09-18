b65d25228f0981119e232e39ccdebcde37193a96
Samuel Rowe <samrowe@deneb.co.uk>
2017-09-18 09:21:27 +0100

    Have i got my assingments shortcut working propperly for python

--- python2/koans/about_scoring_project.py
+++ python2/koans/about_scoring_project.py
@@ -38,12 +38,58 @@ def score(dice):
     if len(dice) == 0:
       return 0
     score = 0
-    if len(dice) == 1:
-      if dice[0] == 5:
-        score = 50
-      if dice[0] == 1:
-        score = 100
-    
+    ones = 0
+    twos = 0
+    threes = 0
+    fours = 0
+    fives = 0
+    sixs = 0
+    severns = 0
+    eights = 0
+    nines = 0
+    for die in dice:
+      if die == 1:
+        ones += 1
+      if die == 2:
+        twos += 1
+      if die == 3:
+        threes += 1
+      if die == 4:
+        fours += 1
+      if die == 5:
+        fives += 1
+      if die == 6:
+        sixes += 1
+      if die == 7:
+        severns += 1
+      if die == 8:
+        eights += 1
+      if die == 9:
+        nines += 1
+    if ones >= 3:
+      ones -= 3
+      score += 1000
+    if twos >= 3:
+      score += 200
+    if threess >= 3:
+      score += 300
+    if fours >= 3:
+      score += 400
+    if fives >= 3:
+      fives -= 3
+      score += 500
+    if sixes >= 3:
+      score += 600
+    if severns >= 3:
+      score += 700
+    if eights >= 3:
+      score += 800
+    if nines >= 3:
+      score += 900
+    if ones >= 0:
+      score = score + ( ones * 100 )
+    if fives >= 0:
+      score = score + (fives * 50)               
     return score
 
 
